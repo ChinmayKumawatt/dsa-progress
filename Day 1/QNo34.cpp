81ff69e21941e2dcccd9826bfba0875cee53234f
ChinmayKumawatt <chinmaykumawat1105@gmail.com>
2026-03-18 12:40:28 +0530

    Question Number 34 First and Last element of a sorted array

---
+++ Day 1/QNo34.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int s = 0;
+        int e = nums.size() -1;
+        int mid = s + (e-s)/2;
+        vector<int> ans = {-1,-1};
+        while(s<=e){
+            if(nums[mid]==target){
+                ans[0] = mid;
+                e = mid-1;
+            }
+            else if(nums[mid]<target){
+                s = mid+1;
+            }
+            else{
+                e = mid -1;
+            }
+            mid = s + (e-s)/2;
+        }
+
+
+        s=0;
+        e = nums.size() -1;
+        mid = s + (e-s)/2;
+        while(s<=e){
+            if(nums[mid]==target){
+                ans[1] = mid;
+                s= mid+1;
+            }
+            else if(nums[mid]<target){
+                s = mid+1;
+            }
+            else{
+                e = mid -1;
+            }
+            mid = s + (e-s)/2;
+        }
+        return ans;
+    }
+};
